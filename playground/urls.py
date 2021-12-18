8c915e7a965cd07a3f94eb80cec0660a5fe4700e
YassinMahouti <61270540+YassinMahouti@users.noreply.github.com>
2021-12-19 00:12:16 +0100

    New login screen

--- playground/urls.py
+++ playground/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('hello/', views.say_hello),
     path('', views.index, name='index'),
-    path('login/', views.login),
-    path('registration/', views.registration),
+
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('registration/', views.registrationPage, name='registration'),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre
 from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def say_hello(request):
     return render(request, 'hello.html', {'name': 'Yassin'})
 
 
+@login_required(login_url='login')
 def index(request):
     """View function for home page of site."""
 
@@ -33,19 +37,38 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 
-def login(request):
+def loginPage(request):
     # form = UserCreationForm()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username or password is incorrect')
     context = {}
     return render(request, 'login.html')
 
 
-def registration(request):
+def registrationPage(request):
     form = CreateUserForm()
     # Handle user registration (Unique Username, hash password ...)
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, user + ' registered successfully')
+            return redirect('login')
 
     context = {'form': form}
     return render(request, 'registration.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
